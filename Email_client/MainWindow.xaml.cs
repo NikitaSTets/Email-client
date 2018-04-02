0f525b053d2f3fa22cb20b591395eddf4a4c6e90
NIKITA <nikita.stets.999@mail.ru>
2018-04-02 21:26:01 +0300

    create IMapService,added Login,Initialize in ImapService

---
+++ Email_client/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Windows;
+using System.Windows.Controls;
+using Email_client.Map;
+
+
+
+namespace Email_client
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private static Frame MainFrame { get; set; }
+        public static bool LoggedIn { get; set; }
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+            MainFrame = mainFrame;          
+            //MainFrame.Content = new LoginPage();        
+            IMapService.Initialize();
+        }
+    }
+}
