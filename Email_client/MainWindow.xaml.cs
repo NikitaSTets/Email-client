26213d2d69f7e4e4f2e4fc0429aaab62f9c940d2
NIKITA <nikita.stets.999@mail.ru>
2018-04-10 21:13:54 +0600

    added smtp client

--- Email_client/MainWindow.xaml.cs
+++ Email_client/MainWindow.xaml.cs
@@ -1,33 +1,79 @@
-﻿
+﻿using System;
 using System.Windows;
 using System.Windows.Controls;
-using Email_client.Model;
+using System.Net;
+using System.Net.Mail;
 
-
+using Microsoft.Win32;
 
 namespace Email_client
-{
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
+{ 
     public partial class MainWindow : Window
     {
-
-        private static Frame MainFrame { get; set; }
-        public static bool LoggedIn { get; set; }
+        OpenFileDialog ofAttachment;
+        string fileName;
+ 
         public MainWindow()
         {
-            
-            InitializeComponent();
-           // MainFrame = mainFrame;          
-            //MainFrame.Content = new LoginPage();        
-            IMapService.Initialize();
-          
+            InitializeComponent();        
         }
 
-        private void button_Click(object sender, RoutedEventArgs e)
+        private void addButton_Click(object sender, RoutedEventArgs e)
         {
+           
+            try
+            {
+                ofAttachment = new OpenFileDialog();
+                ofAttachment.Filter = "Images(.jpg,.png)|*.png;*.jpg;|Pdf Files|*.pdf";
+                Nullable<bool> result = ofAttachment.ShowDialog();
+                if (result==true)
+                {
+                    fileName = ofAttachment.FileName;
+                }
+            }
+            catch (Exception)
+            {
 
+                throw;
+            }
+        }
+
+        private void sendButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //Smtp client details
+                //gmail>>smtp server :smtp.gmail.com,port 587,ssl required
+                SmtpClient clientDetails = new SmtpClient();
+                clientDetails.Port = Convert.ToInt32(clientParametrsPortTextBox.Text.Trim());
+                clientDetails.Host = clientParametrsSmtpServerTextBox.Text.Trim();
+                Nullable<bool> result = clientParametrsProtocolSSICheckBox.IsChecked;
+                clientDetails.EnableSsl = result.Value;//?????
+                clientDetails.DeliveryMethod = SmtpDeliveryMethod.Network;
+                clientDetails.UseDefaultCredentials = false;
+                //message detail
+                clientDetails.Credentials = new NetworkCredential(fromEmailTextBox.Text.Trim(), fromPasswordTextBox.Password.Trim());
+                MailMessage mailDetails = new MailMessage();
+                mailDetails.From = new MailAddress(fromEmailTextBox.Text.Trim());
+                mailDetails.To.Add(toEmailTextBox.Text.Trim());
+                //for multiple reciptionists
+                //mailDetails.To.Add("another recipient email address")
+                //for bcc
+                //mailDetail.Bcc.Add("bcc email address");
+                mailDetails.Subject = toEmailTextBox.Text.Trim();
+                mailDetails.Body = contentOfMessageSubHeaderText.Text.Trim();
+                if (fileName.Length > 0)
+                {
+                    Attachment attachment = new Attachment(fileName);
+                    mailDetails.Attachments.Add(attachment);
+                }
+                clientDetails.Send(mailDetails);
+                fileName = "";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
